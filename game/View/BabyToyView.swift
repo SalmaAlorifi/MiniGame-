b15a405ef6f42c86dd30887a41e640620389cdf4
salmadwv101 <sfalorifi22@twq.idserve.net>
2023-01-01 12:15:12 +0300

    first Commit

---
+++ game/View/BabyToyView.swift
@@ -0,0 +1,75 @@
+//
+//  BabyToyView.swift
+//  game
+//
+//  Created by salma alorifi on 10/06/1444 AH.
+//
+
+import SwiftUI
+
+struct BabyToyView: View {
+    @StateObject private var viewModel = ToyViewModel()
+    let gridItems = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+    ]
+    
+    var drag: some Gesture {
+        DragGesture()
+            .onChanged { state in
+                viewModel.update(dragPosition: state.location)
+            }
+            .onEnded { state in
+                viewModel.update(dragPosition: state.location)
+                withAnimation {
+                    viewModel.confirmWhereToyWasDropped()
+                }
+            }
+    }
+    
+    var body: some View {
+        ZStack {
+            LazyVGrid(columns: gridItems, spacing: 100) {
+                ForEach(viewModel.toyContainers, id: \.id) { toy in
+                    ToyContainer(
+                        toy: toy,
+                        viewModel: viewModel
+                    )
+                }
+            }
+            if let currentToy = viewModel.currentToy {
+                DraggableToy(
+                    toy: currentToy,
+                    position: viewModel.currentPosition,
+                    gesture: drag
+                )
+                    .opacity(viewModel.draggableToyOpacity)
+            }
+        }
+        .onAppear {
+            viewModel.setNextToy()
+        }
+        .alert(
+     
+            Text("Congratulations, you won! 🎉"),
+            isPresented: $viewModel.isGameOver,
+            actions: {
+                Button("Close") {
+                    withAnimation {
+                        viewModel.generateNewGame()
+                    }
+                }
+            },
+            message: {
+                Text("Number of attemps: \(viewModel.attempts)")
+            }
+        )
+    }
+}
+
+struct BabyToyView_Previews: PreviewProvider {
+    static var previews: some View {
+        BabyToyView()
+    }
+}
---
+++ game/gameApp.swift
@@ -0,0 +1,19 @@
+//
+//  gameApp.swift
+//  game
+//
+//  
+//
+
+import SwiftUI
+
+@main
+struct gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            BabyToyView()
+           
+            //ToyContainer()
+        }
+    }
+}
